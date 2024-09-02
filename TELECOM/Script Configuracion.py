34e64d35cdebe08828a0d4c8ac65524ebce4c958
Kevin Ramos <117946754+kramos213@users.noreply.github.com>
2024-09-02 14:25:47 -0500

    Actualizacion de Script

--- TELECOM/Script Configuracion.py
+++ TELECOM/Script Configuracion.py
@@ -2,88 +2,194 @@ import paramiko
 import datetime
 import os
 import time
+import concurrent.futures
+import csv
+from loguru import logger
 
 # Diccionario con IPs organizadas por piso o área
 switch_ips_by_area = {
-     "PSO": ["ip del host"],
-    # Agrega más pisos o áreas según sea necesario
+    "P06-IT": ["172.16.0.201", "172.16.0.224"]
+    # Agrega más áreas según sea necesario
 }
 
-# Credenciales de acceso a los switches
-username = 'xxx'
-password = 'xxx'
+# Carpeta para guardar los logs
+log_folder = "C:/Users/kramos/Desktop/Scritp de Respaldos/Log config"
+os.makedirs(log_folder, exist_ok=True)
 
-# Especifica la carpeta donde se guardarán los logs
-log_folder = "C:/Users/kramos/Desktop/Scritp de Respaldos/Log config"  # Cambia esta ruta por la ruta de tu carpeta de logs
+# Configuración de loguru
+log_filename = os.path.join(log_folder, f"log_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.log")
+logger.add(log_filename, format="{time} {level} {message}", level="INFO")
 
-# Asegúrate de que la carpeta exista; si no, créala
-if not os.path.exists(log_folder):
-    os.makedirs(log_folder)
+def execute_command(shell, command, delay=2):
+    shell.send(f'{command}\n')
+    time.sleep(delay)
+    output = shell.recv(65535).decode()
+    return output
 
-# Nombre del archivo de log basado en la fecha y hora actual
-log_filename = os.path.join(log_folder, f"log_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.txt")
+def validate_vlan_id(vlan_id):
+    return vlan_id.isdigit() and 1 <= int(vlan_id) <= 4095
 
-# Función para registrar mensajes en el archivo de log
-def log_message(message):
-    with open(log_filename, 'a') as log_file:
-        log_file.write(message + '\n')
-
-def execute_commands(switch_ip, area, commands):
+def manage_switch(switch_ip, area, action, **kwargs):
+    #results = []
     try:
         # Crear una instancia SSHClient
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         
         # Conectarse al switch
-        ssh.connect(switch_ip, username=username, password=password)
+        ssh.connect(switch_ip, username=kwargs['ssh_username'], password=kwargs['ssh_password'])
         
         # Crear una instancia de transporte para el canal
         shell = ssh.invoke_shell()
         time.sleep(2)  # Esperar para que la conexión se estabilice
         
-        # Ejecutar los comandos
-        for command in commands:
-            shell.send(f'{command}\n')
-            time.sleep(2)  # Ajustar el tiempo según sea necesario
-            output = shell.recv(65535).decode()
+        if action == 'create_vlan':
+            vlan_id = kwargs['vlan_id']
+            vlan_name = kwargs['vlan_name']
+            if not validate_vlan_id(vlan_id):
+                logger.error(f"ID de VLAN '{vlan_id}' no válido en {switch_ip} ({area}).")
+                results.append((switch_ip, area, action, f"ID de VLAN '{vlan_id}' no válido"))
+                return
+            execute_command(shell, 'enable', delay=2)
+            execute_command(shell, 'conf t', delay=2)
+            execute_command(shell, 'vlan database', delay=2)
+            execute_command(shell, f'vlan {vlan_id} name {vlan_name}', delay=2)
+            execute_command(shell, 'end', delay=2)
+            execute_command(shell, 'write memory', delay=5)
+            logger.info(f"VLAN '{vlan_name}' (ID: {vlan_id}) creada en {switch_ip} ({area}).")
+            results.append((switch_ip, area, action, f"VLAN '{vlan_name}' (ID: {vlan_id}) creada"))
 
-            # Mensaje de éxito o fallo
-            if output:
-                message = f"Comando '{command}' ejecutado con éxito en {switch_ip} ({area}):\n{output}"
-            else:
-                message = f"Error al ejecutar comando '{command}' en {switch_ip} ({area})."
-            
-            # Imprimir y guardar en el log
-            print(message)
-            log_message(message)
-        
-        # Cerrar la conexión
+        elif action == 'rename_port':
+            port = kwargs['port']
+            new_name = kwargs['new_name']
+            execute_command(shell, 'enable', delay=2)
+            execute_command(shell, 'conf t', delay=2)
+            execute_command(shell, f'interface {port}', delay=2)
+            execute_command(shell, f'description {new_name}', delay=2)
+            execute_command(shell, 'end', delay=2)
+            execute_command(shell, 'write memory', delay=5)
+            logger.info(f"Puerto '{port}' renombrado a '{new_name}' en {switch_ip} ({area}).")
+            results.append((switch_ip, area, action, f"Puerto '{port}' renombrado a '{new_name}'"))
+
+        elif action == 'toggle_port':
+            port = kwargs['port']
+            state = kwargs['state']  # 'shutdown' or 'no shutdown'
+            execute_command(shell, 'enable', delay=2)
+            execute_command(shell, 'conf t', delay=2)
+            execute_command(shell, f'interface {port}', delay=2)
+            execute_command(shell, state, delay=2)
+            execute_command(shell, 'end', delay=2)
+            execute_command(shell, 'write memory', delay=5)
+            logger.info(f"Puerto '{port}' en {switch_ip} ({area}) ha sido {'apagado' if state == 'shutdown' else 'encendido'}.")
+            results.append((switch_ip, area, action, f"Puerto '{port}' {'apagado' if state == 'shutdown' else 'encendido'}"))
+
+        elif action == 'find_mac':
+            mac_address = kwargs['mac_address']
+            output = execute_command(shell, 'enable', delay=2)
+            output += execute_command(shell, f'sh mac add | inc {mac_address}', delay=5)
+            logger.info(f"Resultado de la búsqueda de MAC '{mac_address}' en {switch_ip} ({area}):\n{output}")
+            results.append((switch_ip, area, action, f"Resultado de búsqueda de MAC: {output}"))
+
+        elif action == 'config_port':
+            port_conf = kwargs['port_conf']
+            output = execute_command(shell, 'enable', delay=2)
+            output += execute_command(shell, f'sh run int {port_conf}', delay=5)
+            logger.info(f"Resultado de la configuración del '{port_conf}' en {switch_ip} ({area}):\n{output}")
+            results.append((switch_ip, area, action, f"Resultado de configuración: {output}"))
+
         ssh.close()
-    
+    except paramiko.AuthenticationException:
+        logger.error(f"Error de autenticación al conectar con {switch_ip} ({area}). Verifica las credenciales.")
+        results.append((switch_ip, area, action, "Error de autenticación"))
     except Exception as e:
-        error_message = f"Ocurrió un error con {switch_ip} en {area}: {e}"
-        print(error_message)
-        log_message(error_message)
-
-def execute_commands_on_all_switches(commands):
-    for area, ips in switch_ips_by_area.items():
-        start_message = f"Iniciando ejecución de comandos en switches en {area}:"
-        print(start_message)
-        log_message(start_message)
-        
-        for ip in ips:
-            execute_commands(ip, area, commands)
-        
-        end_message = f"Ejecución de comandos completada para {area}.\n"
-        print(end_message)
-        log_message(end_message)
+        logger.error(f"Ocurrió un error al realizar la acción '{action}' en {switch_ip} ({area}): {e}")
+        results.append((switch_ip, area, action, f"Error: {e}"))
+    finally:
+        return results
 
 if __name__ == "__main__":
-    # Definir los comandos a ejecutar
-    commands = [
-        'en',
-        'wr'  # Agregar el comando 'wr' al final para guardar los cambios
-    ]
+    actions = {
+        1: 'create_vlan',
+        2: 'rename_port',
+        3: 'toggle_port',
+        4: 'find_mac',
+        5: 'remove',
+        6: 'modify',
+        7: 'create',
+        8: 'config_port',
+        9: 'exit'
+    }
     
-    # Ejecutar los comandos en todos los switches
-    execute_commands_on_all_switches(commands)
+    action_descriptions = {
+        1: "Crear VLAN",
+        2: "Renombrar Puerto",
+        3: "Encender/Apagar Puerto",
+        4: "Buscar Dirección MAC",
+        5: "Eliminar Usuario",
+        6: "Modificar Usuario",
+        7: "Crear Usuario",
+        8: "Configuración de Puerto",
+        9: "Salir"
+    }
+
+    while True:
+        print("\nSeleccione una acción:")
+        for num, description in action_descriptions.items():
+            print(f"{num}. {description}")
+        
+        try:
+            action_index = int(input("\nIngrese el número de la acción deseada: ").strip())
+            if action_index not in actions:
+                raise ValueError("Número de acción no válido.")
+
+            action = actions[action_index]
+            
+            if action == 'exit':
+                print("Saliendo del programa.")
+                break
+
+            ssh_username = 'TPC'
+            ssh_password = 'Tpc2020*'
+            kwargs = {'ssh_username': ssh_username, 'ssh_password': ssh_password}
+
+            if action == 'create_vlan':
+                kwargs['vlan_id'] = input("Ingrese el ID de la VLAN: ")
+                kwargs['vlan_name'] = input("Ingrese el nombre de la VLAN: ")
+
+            elif action == 'rename_port':
+                kwargs['port'] = input("Ingrese el puerto a renombrar (e.g., port1.0.x): ")
+                kwargs['new_name'] = input("Ingrese el nuevo nombre del puerto: ")
+
+            elif action == 'toggle_port':
+                kwargs['port'] = input("Ingrese el puerto a apagar/encender (e.g., port1.0.x): ")
+                state = input("¿Desea apagar o encender el puerto? (apagar/encender): ").strip().lower()
+                kwargs['state'] = 'shutdown' if state == 'apagar' else 'no shutdown'
+
+            elif action == 'find_mac':
+                kwargs['mac_address'] = input("Ingrese la dirección MAC a buscar: ")
+            
+            elif action == 'config_port':
+                kwargs['port_conf'] = input("Ingrese el puerto a buscar la configuración: ")
+
+            elif action in ['remove', 'modify', 'create']:
+                kwargs['username'] = input("Ingrese el nombre del usuario: ")
+
+            results = []
+            with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+                future_to_ip = {
+                    executor.submit(manage_switch, switch_ip, area, action, **kwargs): (switch_ip, area)
+                    for area, ips in switch_ips_by_area.items()
+                    for switch_ip in ips
+                }
+                for future in concurrent.futures.as_completed(future_to_ip):
+                    switch_ip, area = future_to_ip[future]
+                    try:
+                        result = future.result()
+                        results.extend(result)
+                    except Exception as exc:
+                        logger.error(f"{switch_ip} ({area}) generó una excepción: {exc}")
+
+        except ValueError as e:
+            print(f"Error: {e}")
+        except Exception as e:
+            print(f"Se produjo un error: {e}")
--- TELECOM/Script Log de Sw.py
+++ TELECOM/Script Log de Sw.py
@@ -2,113 +2,135 @@ import paramiko
 import datetime
 import os
 import time
-import re
+from loguru import logger
 
 # Diccionario con IPs organizadas por piso o área
 switch_ips_by_area = {
-    "area": ["ip del host"],
-
-    # Agrega más pisos o áreas según sea necesario
+    "PSO": ["172.16.0.151", "172.16.0.150", "172.16.0.118", "172.16.0.101", "172.16.0.147", "172.16.0.148", "172.16.0.116"],
+    "PPB": ["172.16.0.105", "172.16.0.103", "172.16.0.104", "172.16.0.142", "172.16.0.143", "172.16.0.106", "172.16.0.107"],
+    "P06": ["172.16.0.108", "172.16.0.109", "172.16.0.202", "172.16.0.140", "172.16.0.146"],
+    "P07": ["172.16.0.110", "172.16.0.111", "172.16.0.112", "172.16.0.141", "172.16.0.115"],
+    "P08": ["172.16.0.113", "172.16.0.114"],
+    "P09": ["172.16.0.120", "172.16.0.121", "172.16.0.119"],
+    "P10": ["172.16.0.123", "172.16.0.124"],
+    "P11": ["172.16.0.125", "172.16.0.137", "172.16.0.126", "172.16.0.139"],
+    "P14": ["172.16.0.127", "172.16.0.128"],
+    "P16": ["172.16.0.129", "172.16.0.130", "172.16.0.138"],
+    "P17": ["172.16.0.131", "172.16.0.132", "172.16.0.136"],
+    "P18": ["172.16.0.133", "172.16.0.134", "172.16.0.135"],
+    "P24": ["172.16.0.200", "172.16.0.145"]
 }
 
+# Carpeta base para guardar el log
+base_log_folder = "C:/Users/kramos/Desktop/Log de Script/Log Sw"
+os.makedirs(base_log_folder, exist_ok=True)
 
-# Credenciales de acceso a los switches
-username = 'xx'
-password = 'xxxx'
+# Archivo único para todos los logs
+log_filename = os.path.join(base_log_folder, f"consolidated_log_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.txt")
+logger.add(log_filename, format="{time} {level} {message}", level="INFO")
 
-# Especifica la carpeta donde se guardarán los logs
-log_folder = "C:/Users/kramos/Desktop/Scritp de Respaldos/Log Sw"
-
-if not os.path.exists(log_folder):
-    os.makedirs(log_folder)
-
-log_filename = os.path.join(log_folder, f"log_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.txt")
-
-def log_message(message):
-    with open(log_filename, 'a') as log_file:
-        log_file.write(message + '\n')
-
-def extract_datetime_from_log(log_entry):
+def execute_command(shell, command, delay=2):
     """
-    Extrae la fecha y hora de una entrada de log.
-    Ajusta el patrón de acuerdo al formato de tu log.
-    """
-    pattern = r"(\w+\s+\d+\s+\d+:\d+:\d+)"  # Ejemplo: Aug 31 15:45:30
-    match = re.search(pattern, log_entry)
-    if match:
-        log_time_str = match.group(1)
-        log_time = datetime.datetime.strptime(log_time_str, "%b %d %H:%M:%S")
-        # Ajustar el año actual
-        log_time = log_time.replace(year=datetime.datetime.now().year)
-        return log_time
-    return None
-
-def analyze_log(output):
-    events = []
-    now = datetime.datetime.now()
-    lines = output.splitlines()
-
-    # Palabras clave para eventos importantes
-    important_keywords = ["error", "fail", "critical", "warning", "down", "up", "offline"]
-
-    for line in lines:
-        log_time = extract_datetime_from_log(line)
-        
-        if log_time and (now - log_time).days < 1:
-            if any(keyword in line.lower() for keyword in important_keywords):
-                events.append(line)
+    Envía un comando al shell del switch y retorna la salida.
     
-    return events
+    Args:
+        shell (paramiko.Channel): El canal SSH del switch.
+        command (str): El comando a ejecutar.
+        delay (int): Tiempo en segundos para esperar la respuesta.
 
-def execute_commands(switch_ip, area, commands):
+    Returns:
+        str: La salida del comando.
+    """
+    shell.send(f'{command}\n')
+    time.sleep(delay)
+    output = shell.recv(65535).decode()
+    return output
+
+def analyze_logs(output, keywords):
+    """
+    Analiza la salida de los logs y filtra las entradas recientes basadas en palabras clave.
+    
+    Args:
+        output (str): La salida del comando 'show log'.
+        keywords (list): Lista de palabras clave para filtrar los logs.
+
+    Returns:
+        list: Lista de entradas de log relevantes.
+    """
+    log_entries = []
+    cutoff_date = datetime.datetime.now() - datetime.timedelta(days=2)  # Fecha límite para los logs
+    
+    for line in output.splitlines():
+        if any(keyword in line.lower() for keyword in keywords):
+            try:
+                # Ajusta estos valores según el formato de tus logs
+                log_time_str = line[:19]  # Asegúrate de que esto es correcto para tu formato de fecha y hora
+                log_time = datetime.datetime.strptime(log_time_str, '%Y %b %d %H:%M:%S')
+                
+                if log_time >= cutoff_date:
+                    log_entries.append(line)
+            except ValueError:
+                # No se pudo parsear la fecha en el log
+                logger.debug(f"No se pudo parsear la fecha en el log: {line}")
+                continue
+
+    logger.debug(f"Log entries filtrados: {log_entries}")
+    return log_entries
+
+def fetch_and_analyze_logs(switch_ip, area, ssh_username, ssh_password, keywords):
+    """
+    Conecta al switch, ejecuta comandos y analiza los logs.
+    
+    Args:
+        switch_ip (str): La IP del switch.
+        area (str): El área correspondiente al switch.
+        ssh_username (str): Nombre de usuario SSH.
+        ssh_password (str): Contraseña SSH.
+        keywords (list): Lista de palabras clave para filtrar los logs.
+    """
     try:
+        logger.info(f"Conectando al switch {switch_ip} en el área {area}.")
+        
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(switch_ip, username=username, password=password)
+        ssh.connect(switch_ip, username=ssh_username, password=ssh_password)
         shell = ssh.invoke_shell()
         time.sleep(2)
-        
-        for command in commands:
-            shell.send(f'{command}\n')
-            time.sleep(5)
-            output = shell.recv(65535).decode()
 
-            events = analyze_log(output)
-            if events:
-                for event in events:
-                    message = f"Evento importante detectado en {switch_ip} ({area}): {event}"
-                    print(message)
-                    log_message(message)
-            else:
-                message = f"No se detectaron eventos importantes en {switch_ip} ({area})."
-                print(message)
-                log_message(message)
+        execute_command(shell, 'enable', delay=2)  # Activar modo privilegiado
+        execute_command(shell, 'terminal length 0', delay=2)  # Configurar salida sin paginación
+        output_logs = execute_command(shell, 'show log', delay=5)  # Ejecutar el comando para obtener logs
         
+        # Agrega este log para verificar el contenido de los logs
+        logger.debug(f"Output logs desde {switch_ip} ({area}): {output_logs}")
+        
+        log_entries = analyze_logs(output_logs, keywords)
+
+        # Escribir encabezado para el área
+        logger.info(f"\n\n{'='*50}\nÁrea: {area}\n{'='*50}\n")
+
+        if log_entries:
+            for entry in log_entries:
+                logger.info(f"Log encontrado en {switch_ip}: {entry}")
+        else:
+            logger.info(f"No se encontraron entradas de log relevantes en {switch_ip} ({area}).")
+
         ssh.close()
-    
-    except Exception as e:
-        error_message = f"Ocurrió un error con {switch_ip} en {area}: {e}"
-        print(error_message)
-        log_message(error_message)
 
-def execute_commands_on_all_switches(commands):
-    for area, ips in switch_ips_by_area.items():
-        start_message = f"Iniciando ejecución de comandos en switches en {area}:"
-        print(start_message)
-        log_message(start_message)
-        
-        for ip in ips:
-            execute_commands(ip, area, commands)
-        
-        end_message = f"Ejecución de comandos completada para {area}.\n"
-        print(end_message)
-        log_message(end_message)
+    except paramiko.AuthenticationException:
+        logger.error(f"Error de autenticación al conectar con {switch_ip} ({area}). Verifica las credenciales.")
+    except Exception as e:
+        logger.error(f"Ocurrió un error al obtener y analizar los logs en {switch_ip} ({area}): {e}")
 
 if __name__ == "__main__":
-    commands = [
-        'en',
-        'terminal length 0',
-        'sh log'
-    ]
-    
-    execute_commands_on_all_switches(commands)
+    keywords = ['error', 'down', 'up', 'warning']  # Palabras clave para filtrar los logs.
+    ssh_username = 'TPC'
+    ssh_password = 'Tpc2020*'
+
+    # Procesar cada IP de manera secuencial por área
+    for area, ips in switch_ips_by_area.items():
+        # Agregar encabezado para el área en el archivo de log
+        logger.info(f"\n\n{'='*50}\nÁrea: {area}\n{'='*50}\n")
+        
+        for ip in ips:
+            fetch_and_analyze_logs(ip, area, ssh_username, ssh_password, keywords)
