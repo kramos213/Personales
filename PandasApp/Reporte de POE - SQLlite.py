9e2994a4001bad6c481c71b1414e704eab18234e
Kevin Ramos <117946754+kramos213@users.noreply.github.com>
2024-11-30 16:44:21 -0500

    correciones

--- PandasApp/Reporte de POE - SQLlite.py
+++ PandasApp/Reporte de POE - SQLlite.py
@@ -9,39 +9,48 @@ if platform.system() == "Windows":
     database_path = "Z:/Monitoreo de red/DataWarehouse/DataWarehouse.db"
     # Definir la carpeta con los archivos CSV
     carpeta = "Z:/Monitoreo de red/Poe Puertos"
+    processed_files_path = "Z:/Monitoreo de red/DataWarehouse/processed_files.txt"
 else:  # macOS o Linux
     database_path = "/Volumes/Sophos/Monitoreo de red/DataWarehouse/DataWarehouse.db"
     carpeta = "/Volumes/Sophos/Monitoreo de red/Poe Puertos"
+    processed_files_path = "/Volumes/Sophos/Monitoreo de red/DataWarehouse/processed_files.txt"
+
 
 # Obtener todos los archivos CSV en la carpeta
 archivos_csv = [os.path.join(carpeta, f) for f in os.listdir(carpeta) if f.endswith('.csv')]
+
+# Leer archivos procesados previamente
+if os.path.exists(processed_files_path):
+    with open(processed_files_path, 'r') as file:
+        archivos_procesados = set(file.read().splitlines())
+else:
+    archivos_procesados = set()
+
+# Filtrar archivos que aún no se han procesado
+archivos_pendientes = [archivo for archivo in archivos_csv if archivo not in archivos_procesados]
 
 # Crear una lista para almacenar los DataFrames
 df_lista = []
 
 # Función para detectar codificación de archivos CSV
-def detectar_codificacion(archivo):
-    with open(archivo, 'rb') as file:
-        raw_data = file.read()
-        resultado = chardet.detect(raw_data)
-        return resultado['encoding']
+def detectar_codificacion(file_path):
+    with open(file_path, 'rb') as f:
+        rawdata = f.read(10000)  # Leer los primeros 10 KB
+    resultado = chardet.detect(rawdata)
+    return resultado['encoding']
 
 # Función para limpiar y filtrar el DataFrame
 def limpiar_df(df):
-    # Filtrar las columnas necesarias
     columnas = ['Fecha y Hora', 'Área', 'Switch IP', 'Interface', 'Admin', 'Pri', 'Oper', 'Power (mW)', 'Device', 'Class', 'Max (mW)']
     df = df[columnas]
-    
-    # Limpiar filas vacías o con valores no deseados
     df = df.dropna(subset=columnas, how='all')  # Eliminar filas donde todas las columnas están vacías
     df = df[df['Interface'] != 'Interface']  # Eliminar filas con 'Interface' como valor en la columna 'Interface'
-    df = df[df['Oper'] != 'Off']  # Eliminar filas con 'Interface' como valor en la columna 'Interface'
+    df = df[df['Oper'] != 'Off']  # Eliminar filas con 'Oper' como 'Off'
     df = df.drop(columns=['Device'])
-
     return df
 
-# Leer los archivos CSV y concatenarlos
-for i, archivo in enumerate(archivos_csv, 1):
+# Leer los archivos CSV pendientes y concatenarlos
+for i, archivo in enumerate(archivos_pendientes, 1):
     try:
         # Detectar la codificación
         encoding = detectar_codificacion(archivo)
@@ -50,10 +59,9 @@ for i, archivo in enumerate(archivos_csv, 1):
         df = pd.read_csv(archivo, encoding=encoding)
         
         # Mostrar información sobre el archivo
-        print(f"Leyendo archivo {i}/{len(archivos_csv)}: {archivo}")
+        print(f"Leyendo archivo {i}/{len(archivos_pendientes)}: {archivo}")
         print(f"  - Codificación detectada: {encoding}")
         print(f"  - Columnas en el archivo: {df.columns.tolist()}")
-        #print(f"  - Primeras filas:\n{df.head()}")
         
         # Limpiar el DataFrame y agregarlo a la lista
         df_lista.append(limpiar_df(df))
@@ -62,43 +70,51 @@ for i, archivo in enumerate(archivos_csv, 1):
         continue  # Continúa con el siguiente archivo en caso de error
 
 # Concatenar todos los DataFrames
-df_combinado = pd.concat(df_lista, ignore_index=True)
+if df_lista:
+    df_combinado = pd.concat(df_lista, ignore_index=True)
 
-# Conectar a la base de datos
-engine = create_engine(f'sqlite:///{database_path}')
+    # Conectar a la base de datos
+    engine = create_engine(f'sqlite:///{database_path}')
 
-# Verificar si la tabla 'Reporte_POE' existe antes de cargar los datos
-with engine.connect() as connection:
+    # Verificar si la tabla 'Reporte_POE' existe antes de cargar los datos
+    with engine.connect() as connection:
+        try:
+            query = text("SELECT name FROM sqlite_master WHERE type='table' AND name='Reporte_POE';")
+            result = connection.execute(query)
+            table_exists = result.fetchone()
+            
+            if table_exists:
+                print("La tabla 'Reporte_POE' ya existe.")
+            else:
+                print("La tabla 'Reporte_POE' no existe, creando tabla nueva.")
+                # Crear tabla si no existe
+                df_combinado.head(0).to_sql('Reporte_POE', engine, if_exists='replace', index=False)
+        except Exception as e:
+            print(f"Error al verificar la existencia de la tabla: {e}")
+            exit()
+
+    # Cargar los datos en la tabla del Data Warehouse
     try:
-        query = text("SELECT name FROM sqlite_master WHERE type='table' AND name='Reporte_POE';")
-        result = connection.execute(query)
-        table_exists = result.fetchone()
-        
-        if table_exists:
-            print("La tabla 'Reporte_POE' ya existe.")
-        else:
-            print("La tabla 'Reporte_POE' no existe, creando tabla nueva.")
-            # Crear tabla si no existe
-            df_combinado.head(0).to_sql('Reporte_POE', engine, if_exists='replace', index=False)
+        table_name = "Reporte_POE"
+        df_combinado.to_sql(table_name, engine, if_exists='replace', index=False)
+        print(f"Datos cargados en el Data Warehouse en la tabla '{table_name}'.")
     except Exception as e:
-        print(f"Error al verificar la existencia de la tabla: {e}")
+        print(f"Error al cargar los datos en la base de datos: {e}")
         exit()
 
-# Cargar los datos en la tabla del Data Warehouse
-try:
-    table_name = "Reporte_POE"
-    df_combinado.to_sql(table_name, engine, if_exists='replace', index=False)
-    print(f"Datos cargados en el Data Warehouse en la tabla '{table_name}'.")
-except Exception as e:
-    print(f"Error al cargar los datos en la base de datos: {e}")
-    exit()
+    # Validar que los datos están en la base de datos
+    try:
+        with engine.connect() as connection:
+            query = text(f"SELECT COUNT(*) FROM 'Reporte_POE'")
+            result = connection.execute(query)
+            print(f"Total de registros en el Data Warehouse: {result.scalar()}")
+    except Exception as e:
+        print(f"Error al validar los datos en la base de datos: {e}")
+        exit()
 
-# Validar que los datos están en la base de datos
-try:
-    with engine.connect() as connection:
-        query = text(f"SELECT COUNT(*) FROM 'Reporte_POE'")
-        result = connection.execute(query)
-        print(f"Total de registros en el Data Warehouse: {result.scalar()}")
-except Exception as e:
-    print(f"Error al validar los datos en la base de datos: {e}")
-    exit()
+    # Actualizar la lista de archivos procesados
+    with open(processed_files_path, 'a') as file:
+        for archivo in archivos_pendientes:
+            file.write(f"{archivo}\n")
+else:
+    print("No hay archivos nuevos para procesar.")
--- PandasApp/Reporte de Switches - SQLlite.py
+++ PandasApp/Reporte de Switches - SQLlite.py
@@ -1,14 +1,12 @@
-import pandas as pd
 import os
+import pandas as pd
+from sqlalchemy import create_engine, text
 import chardet
-from sqlalchemy import create_engine
-from sqlalchemy.sql import text
 import platform
 
 # Configurar rutas según el sistema operativo
 if platform.system() == "Windows":
     database_path = "Z:/Monitoreo de red/DataWarehouse/DataWarehouse.db"
-    os.makedirs(os.path.dirname(database_path), exist_ok=True)
     folder_path = "Z:/Monitoreo de red/Reporte de Switches"
     processed_files_path = "Z:/Monitoreo de red/DataWarehouse/processed_files.txt"
 else:  # macOS o Linux
@@ -23,95 +21,93 @@ def detectar_codificacion(file_path):
     resultado = chardet.detect(rawdata)
     return resultado['encoding']
 
-# Cargar lista de archivos procesados
-if os.path.exists(processed_files_path):
-    with open(processed_files_path, 'r') as f:
-        processed_files = set(f.read().splitlines())
-else:
-    processed_files = set()
+# Función para cargar y procesar los archivos CSV
+def procesar_archivos_csv(folder_path, processed_files_path):
+    # Leer lista de archivos procesados
+    if os.path.exists(processed_files_path):
+        with open(processed_files_path, 'r') as file:
+            processed_files = set(file.read().splitlines())
+    else:
+        processed_files = set()
 
-# Obtener archivos no procesados
-all_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith('.csv')]
-unprocessed_files = [f for f in all_files if f not in processed_files]
+    # Obtener lista de archivos no procesados
+    all_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith('.csv')]
+    unprocessed_files = [f for f in all_files if f not in processed_files]
 
-# Salir si no hay archivos nuevos
-if not unprocessed_files:
-    print("No hay archivos nuevos para procesar.")
-    exit()
+    if not unprocessed_files:
+        print("No hay archivos nuevos para procesar.")
+        return None
 
-# Procesar archivos nuevos
-dataframes = []
-for i, file in enumerate(unprocessed_files):
-    print(f"Leyendo archivo {i + 1}/{len(unprocessed_files)}: {file}")
-    try:
-        # Detectar la codificación del archivo
-        encoding = detectar_codificacion(file)
-        print(f"  - Codificación detectada: {encoding}")
-        
-        # Intentar leer el archivo con la codificación detectada
-        df = pd.read_csv(file, encoding=encoding, delimiter=',', on_bad_lines='skip')
-        print(f"  - Columnas detectadas: {df.columns}")
-        dataframes.append(df)
-        
-        # Registrar el archivo como procesado
-        with open(processed_files_path, 'a') as f:
-            f.write(f"{file}\n")
-    except Exception as e:
-        print(f"  - Error al leer el archivo {file}: {e}")
+    dataframes = []
+    for i, file in enumerate(unprocessed_files, 1):
+        print(f"Leyendo archivo {i}/{len(unprocessed_files)}: {file}")
+        try:
+            # Detectar codificación
+            encoding = detectar_codificacion(file)
+            print(f"  - Codificación detectada: {encoding}")
+            
+            # Leer el archivo CSV
+            df = pd.read_csv(file, encoding=encoding, delimiter=',', on_bad_lines='skip')
+            dataframes.append(df)
 
-# Verificar si hay DataFrames consolidados
-if not dataframes:
-    print("\nNo se pudieron consolidar archivos.")
-    exit()
 
-# Concatenar solo los DataFrames exitosos
-df = pd.concat(dataframes, ignore_index=True)
-print("\nConsolidación completada.")
+            # Registrar el archivo como procesado
+            with open(processed_files_path, 'a') as f:
+                f.write(f"{file}\n")
+        except Exception as e:
+            print(f"Error al procesar el archivo {file}: {e}")
 
-# Limpiar y normalizar datos
-df_cleaned = df.dropna()
-df_cleaned['Fecha'] = pd.to_datetime(df_cleaned['Fecha'], errors='coerce')
-df_cleaned.rename(columns=lambda x: x.strip().lower().replace(' ', '_'), inplace=True)
+    if dataframes:
+        return pd.concat(dataframes, ignore_index=True)
+    else:
+        print("No se pudieron consolidar archivos.")
+        return None
 
-# Convertir tipos de datos
-df_cleaned['ip_del_switch'] = df_cleaned['ip_del_switch'].astype(str)
-df_cleaned['área'] = df_cleaned['área'].astype(str)
-df_cleaned['evento'] = df_cleaned['evento'].astype(str)
-df_cleaned['puerto'] = df_cleaned['puerto'].astype(str)
-df_cleaned['detalles'] = df_cleaned['detalles'].astype(str)
+# Función para limpiar y normalizar el DataFrame
+def limpiar_y_normalizar_df(df):
+    df_cleaned = df.dropna()
+    df_cleaned['Fecha'] = pd.to_datetime(df_cleaned['Fecha'], errors='coerce')
+    df_cleaned.rename(columns=lambda x: x.strip().lower().replace(' ', '_'), inplace=True)
 
-# Conectar al Data Warehouse
-engine = create_engine(f'sqlite:///{database_path}')
+    # Convertir tipos de datos
+    df_cleaned['ip_del_switch'] = df_cleaned['ip_del_switch'].astype(str)
+    df_cleaned['área'] = df_cleaned['área'].astype(str)
+    df_cleaned['evento'] = df_cleaned['evento'].astype(str)
+    df_cleaned['puerto'] = df_cleaned['puerto'].astype(str)
+    df_cleaned['detalles'] = df_cleaned['detalles'].astype(str)
+    return df_cleaned
 
-# Verificar la existencia de la tabla
-with engine.connect() as connection:
-    try:
-        query = text("SELECT name FROM sqlite_master WHERE type='table' AND name='Reporte_switches';")
+# Función para cargar datos a la base de datos
+def cargar_datos_bd(df, database_path, table_name):
+    engine = create_engine(f'sqlite:///{database_path}')
+    with engine.connect() as connection:
+        # Verificar si la tabla existe
+        query = text(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}';")
         result = connection.execute(query)
         table_exists = result.fetchone()
+
         if table_exists:
-            print("La tabla 'Reporte Switches' ya existe.")
+            print(f"La tabla '{table_name}' ya existe.")
         else:
-            print("La tabla 'Reporte Switches' no existe, creando tabla nueva.")
+            print(f"La tabla '{table_name}' no existe. Creando tabla nueva.")
+            df.head(0).to_sql(table_name, engine, if_exists='replace', index=False)
+
+    # Cargar datos
+    try:
+        df.to_sql(table_name, engine, if_exists='append', index=False)
+        print(f"Datos cargados en la tabla '{table_name}'.")
     except Exception as e:
-        print(f"Error al verificar la existencia de la tabla: {e}")
-        exit()
+        print(f"Error al cargar los datos en la base de datos: {e}")
+        return
 
-# Cargar los datos en la base de datos
-try:
-    table_name = "Reporte_switches"
-    df_cleaned.to_sql(table_name, engine, if_exists='append', index=False)
-    print(f"Datos cargados en el Data Warehouse en la tabla '{table_name}'.")
-except Exception as e:
-    print(f"Error al cargar los datos en la base de datos: {e}")
-    exit()
-
-# Validar que los datos están en la base de datos
-try:
+    # Validar datos cargados
     with engine.connect() as connection:
         query = text(f"SELECT COUNT(*) FROM {table_name}")
         result = connection.execute(query)
-        print(f"Total de registros en el Data Warehouse: {result.scalar()}")
-except Exception as e:
-    print(f"Error al validar los datos en la base de datos: {e}")
-    exit()
+        print(f"Total de registros en la tabla '{table_name}': {result.scalar()}")
+
+# Main
+df = procesar_archivos_csv(folder_path, processed_files_path)
+if df is not None:
+    df_cleaned = limpiar_y_normalizar_df(df)
+    cargar_datos_bd(df_cleaned, database_path, "Reporte_switches")
